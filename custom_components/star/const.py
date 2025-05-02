8775d840a42f64946690531cbd65223e707e72ca
Sushisteak <florian.lescanf@gmail.com>
2025-05-02 21:47:00 +0200

    Add constants

--- custom_components/star/const.py
+++ custom_components/star/const.py
@@ -1,5 +1,9 @@
 DOMAIN = "star"
 
+# STEP USER
+CONF_API_KEY = "api_key"
+CONF_UPDATE_INTERVAL = "update_interval"
+
 LINE_API_URL = "https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-bus-topologie-li\
     gnes-td/records?select=nomcourt%2Cnomlong&where=nomfamillecommerciale%3D%27CHRONOSTAR%27%20OR%2\
         0%27Urbaine%27%20OR%20%27Inter-quartiers%27%20OR%20%27M%C3%A9tropolitaine%27&order_by=nomco\
