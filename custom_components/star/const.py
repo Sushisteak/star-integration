7d4740dcd4e5b2ee793251c3e3fd17b44c75c1ab
Sushisteak <florian.lescanf@gmail.com>
2025-05-02 22:09:56 +0200

    update const ligne api

--- custom_components/star/const.py
+++ custom_components/star/const.py
@@ -4,11 +4,7 @@ DOMAIN = "star"
 CONF_API_KEY = "api_key"
 CONF_BUS_NUMBER = "bus_number"
 
-LINE_API_URL = "https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-bus-topologie-li\
-    gnes-td/records?select=nomcourt%2Cnomlong&where=nomfamillecommerciale%3D%27CHRONOSTAR%27%20OR%2\
-        0%27Urbaine%27%20OR%20%27Inter-quartiers%27%20OR%20%27M%C3%A9tropolitaine%27&order_by=nomco\
-            urt&limit=100&offset=0&timezone=Europe%2FParis&include_links=false&include_app_metas=fa\
-                lse"
+LINE_API_URL = "https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-bus-topologie-lignes-td/records?select=nomcourt%2Cnomlong&where=nomfamillecommerciale%3D%27CHRONOSTAR%27%20OR%20%27Urbaine%27%20OR%20%27Inter-quartiers%27%20OR%20%27M%C3%A9tropolitaine%27&order_by=nomcourt&limit=100&offset=0&timezone=Europe%2FParis&include_links=false&include_app_metas=false"
 DIRECTIONS_API_URL = "https://data.explore.star.fr/api/explore/v2.1/catalog/datasets/tco-bus-topolo\
     gie-parcours-td/records?select=nomcourtligne%2C%20id%2C%20nomarretarrivee%2C%20libellelong%2C%2\
         0type&where=nomcourtligne%3D%27{ligne}%27&limit=100&offset=0&timezone=UTC&include_links=fal\
