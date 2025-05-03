bcac3fd2060f44d8edab739221716fb20ca4d15b
Sushisteak <florian.lescanf@gmail.com>
2025-05-03 20:03:22 +0200

    Add errors logs for direction and stop

--- custom_components/star/api.py
+++ custom_components/star/api.py
@@ -13,15 +13,15 @@ class StarApi():
     # Récupération des lignes pour l'étape 1
     async def _fetch_bus_lines() -> dict[str, str]:
         """Call STAR API to get all the lines."""
-        _LOGGER.error("Calling _fetch_bus_lines function")
+        _LOGGER.debug("Calling _fetch_bus_lines function")
         async with aiohttp.ClientSession() as session:
             async with session.get(LINE_API_URL) as resp:
                 if resp.status != 200:
                     text = await resp.text()
-                    _LOGGER.error("Lines API error %s: %s", resp.status, text)
+                    _LOGGER.error("Error retrieving bus lines : %s - %s", resp.status, text)
                     return []
                 data = await resp.json()
-                _LOGGER.error("_fetch_bus_lines data : %s", data)
+                _LOGGER.debug("Lines retreived : %s", data)
                 return [
                     (item["nomcourt"], f'{item["nomcourt"]} - {item["nomlong"]}')
                     for item in data.get("results", [])
@@ -30,15 +30,15 @@ class StarApi():
     # Récupération des directions de la ligne choisie pour l'étape 2
     async def _fetch_directions(nomcourt: str) -> list[tuple[str, str]]:
         """Call STAR API to get all direction of one line."""
-        _LOGGER.debug("Fetching directions for line: %s", nomcourt)
+        _LOGGER.debug("Calling _fetch_directions function")
         async with aiohttp.ClientSession() as session:
             async with session.get(DIRECTIONS_API_URL.format(ligne=nomcourt)) as resp:
                 if resp.status != 200:
                     text = await resp.text()
-                    _LOGGER.error("Direction API error %s: %s", resp.status, text)
+                    _LOGGER.error("Error retrieving directions for line %s : %s - %s", nomcourt, resp.status, text)
                     return []
                 data = await resp.json()
-                _LOGGER.debug("Directions API result: %s", data)
+                _LOGGER.debug("Directions for line %s retrevied : %s", nomcourt, data)
                 return [
                     (item["id"], item["libellelong"], item["nomarretarrivee"])
                     for item in data.get("results", [])
@@ -47,12 +47,13 @@ class StarApi():
     # Récupération des arrêts de la ligne choisie pour l'étape 3
     async def _fetch_stops(idparcours: str) -> list[str]:
         """Call STAR API to get all stops of one line."""
-        _LOGGER.debug("Fetching stops for parcours: %s", idparcours)
+        _LOGGER.debug("Calling _fetch_stops function")
         async with aiohttp.ClientSession() as session:
             async with session.get(ARRETS_API_URL.format(parcours=idparcours)) as resp:
                 if resp.status != 200:
                     text = await resp.text()
-                    _LOGGER.error("Stops API error %s: %s", resp.status, text)
+                    _LOGGER.error("Error retrieving stops for parcours %s : %s - %s", idparcours, resp.status, text)
                     return []
                 data = await resp.json()
+                _LOGGER.debug("Stops for parcours %s retrevied : %s", idparcours, data)
                 return [item["nomarret"] for item in data.get("results", [])]
