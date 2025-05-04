053d9845a965a10eb4fba4eaccd7ac769bcdb1b2
Sushisteak <florian.lescanf@gmail.com>
2025-05-04 12:43:30 +0200

    Fix

--- custom_components/star/__init__.py
+++ custom_components/star/__init__.py
@@ -1,15 +1,6 @@
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from .coordinator import StarCoordinator
 from .bus_sensor import BusSensor
 
-async def async_setup_entry(hass, config_entry, async_add_entities: AddEntitiesCallback):
 
-    coordinator = StarCoordinator(hass, config_entry)
-
-    await coordinator.async_config_entry_first_refresh()
-
-    entities = [
-        BusSensor(coordinator, index=0),
-        BusSensor(coordinator, index=1),
-    ]
-    async_add_entities(entities)
+async def async_setup(hass, config):
+    return True
--- custom_components/star/bus_sensor.py
+++ custom_components/star/bus_sensor.py
@@ -4,9 +4,22 @@ import unicodedata
 from datetime import datetime, timezone
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from .coordinator import StarCoordinator
 
 _LOGGER = logging.getLogger(__name__)
+
+async def async_setup_entry(hass, config_entry, async_add_entities: AddEntitiesCallback):
+
+    coordinator = StarCoordinator(hass, config_entry)
+
+    await coordinator.async_config_entry_first_refresh()
+
+    entities = [
+        BusSensor(coordinator, index=0),
+        BusSensor(coordinator, index=1),
+    ]
+    async_add_entities(entities)
 
 def slugify(text: str) -> str:
     """Format names"""
