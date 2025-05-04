50c604dae13a332c2c146e98ba09c4ea30e86c16
Sushisteak <florian.lescanf@gmail.com>
2025-05-04 12:22:06 +0200

    Fix async_add_entities

--- custom_components/star/__init__.py
+++ custom_components/star/__init__.py
@@ -1,8 +1,8 @@
-from .const import DOMAIN
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from .coordinator import StarCoordinator
 from .bus_sensor import BusSensor
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
+async def async_setup_entry(hass, config_entry, async_add_entities: AddEntitiesCallback):
 
     coordinator = StarCoordinator(hass, config_entry)
 
